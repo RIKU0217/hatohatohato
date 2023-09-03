25aff03f5f2337a4779d30fdc76b51fbdec1a98b
RIKU0217 <splmbnesy2015@gmail.com>
2023-09-04 02:21:55 +0900

    長押してる間だけ動くことの実装に失敗

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 const ImageMotion: React.FC = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [keyState, setKeyState] = useState<{ [key: string]: boolean }>({});
-
-  const speed = 5; // 移動速度を調整
+  const [position, setPosition] = useState({ x: 0, y: 0 });//setを使うならuseStateを使っとけ
+  const [keyState, setKeyState] = useState<{ [key: string]: boolean }>({});//setを使うと画面が更新される
+  const realspeed = 10;
+  const reqIdRef = React.useRef<number>();
+  let speed: number = realspeed; // 移動速度を調整
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -24,27 +25,37 @@ const ImageMotion: React.FC = () => {
 
     window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('keyup', handleKeyUp);
+    // キーボードイベントリスナーを設定
+    // window.addEventListener('keydown', (event: KeyboardEvent) => {
+    // const key = event.key; // 押されたキーの文字列
+
+    // // キーが特定の文字列と一致するかを判定
+    // if  (key === 'ArrowUp') {
+    //   // 'ArrowUp' キーが押された場合の処理
+    //   setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y - speed }));
+    //   console.log('ArrowUp キーが押されました');
+    // } else if (key === 'ArrowDown') {
+    //   // 'ArrowDown' キーが押された場合の処理
+    //   console.log('ArrowDown キーが押されました');
+    // } else if (key === 'ArrowLeft') {
+    //   // 'ArrowLeft' キーが押された場合の処理
+    //   console.log('ArrowLeft キーが押されました');
+    // } else if (key === 'ArrowRight') {
+    //   // 'ArrowRight' キーが押された場合の処理
+    //   console.log('ArrowRight キーが押されました');
+    // }
+    // // 他のキーに対する処理を追加できます
+    // });
+
 
     // キーの状態に応じて画像を連続的に移動する関数
-    const moveImage = () => {
-      if (keyState['ArrowUp']) {
-        setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y - speed }));
-      }
-      if (keyState['ArrowDown']) {
-        setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y + speed }));
-      }
-      if (keyState['ArrowLeft']) {
-        setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x - speed }));
-      }
-      if (keyState['ArrowRight']) {
-        setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x + speed }));
-      }
-
-      requestAnimationFrame(moveImage);
-    };
+   
 
     // アニメーションを開始
-    moveImage();
+    if(reqIdRef.current!=undefined){
+      cancelAnimationFrame(reqIdRef.current) 
+    }
+    reqIdRef.current = requestAnimationFrame(moveImage);
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
@@ -52,11 +63,34 @@ const ImageMotion: React.FC = () => {
     };
   }, [keyState]);
 
+  const moveImage = useCallback(() => {
+
+    if (keyState['ArrowUp'] || keyState['w']) {
+      speed = realspeed
+      setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y - speed }));
+    }
+    if (keyState['ArrowDown']|| keyState['s']) {
+      setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y + speed }));
+    }
+    if (keyState['ArrowLeft']|| keyState['a']) {
+      setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x - speed }));
+    }
+    if (keyState['ArrowRight']|| keyState['d']) {
+      setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x + speed }));
+    }
+    console.log(keyState)
+    console.log(position)
+    
+
+    requestAnimationFrame(moveImage);
+  }, [keyState]);
+
   return (
     <div>
       <h1>キーボードの矢印キーで画像を動かす</h1>
+      
       <img
-        src="/sozai/front_hato_pa.png"
+        src="/sozai/front-hato.gif"
         alt="画像の説明"
         style={{
           position: 'absolute',
